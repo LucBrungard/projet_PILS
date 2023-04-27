4ff6e0d67abde7cfb252063a746eb5c6b10e9618
LucBrungard <luc.brungard@laposte.net>
2023-04-27 17:34:05 +0200

    feat: :sparkles: implement cli

--- src/cli/cli.py
+++ src/cli/cli.py
@@ -1,5 +1,27 @@
-class CLI:
-   """Class to handle inputs in cmd"""
+import threading
+from commandParser import parseCommandCLI
+from ivybus import *
 
-   def __init__(self) -> None:
-      pass
+
+class CLI(threading.Thread):
+    """Class to handle inputs in cmd"""
+
+    def __init__(self) -> None:
+        threading.Thread.__init__(self, target=self.readInputs)
+        self.daemon = True
+        self.ivybus = MyAgent("cli")
+        self.stop_event = threading.Event()
+
+    def stop(self):
+        self.ivybus.stop()
+        self.stop_event.set()
+
+    def readInputs(self):
+        while not self.stop_event.is_set():
+            user_input = input()
+            command = parseCommandCLI(user_input)
+            if command != None:
+                self.ivybus.send_msg(command.toLogo())
+            else:
+                print("Commande incorrecte")
+                    
