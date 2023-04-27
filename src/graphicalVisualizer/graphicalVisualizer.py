25160a221f65c99d825f780f95a61f216dc285b2
LucBrungard <luc.brungard@laposte.net>
2023-04-27 17:36:26 +0200

    refactor: :fire: remove useless code

--- src/graphicalVisualizer/graphicalVisualizer.py
+++ src/graphicalVisualizer/graphicalVisualizer.py
@@ -1,57 +1,65 @@
 from tkinter import *
-from ivybus import *
 from graphicalVisualizer.tortue import *
-from queue import *
 from PIL import EpsImagePlugin
 
 
 class GraphicalVisualizer:
     """Class that represent the graphical visualizer"""
 
-    def __init__(self, master: Toplevel, queue: Queue, ivybus: MyAgent, endApplication) -> None:
+    def __init__(self, master: Toplevel) -> None:
         self.master = master
         self.master.geometry("500x250")
         master.title("Visualiseur graphique")
 
-        self.ivybus = ivybus
-
         self.canvasTurtle = Canvas(master, width=600, height=600)
         self.canvasTurtle.grid(row=1, column=1, columnspan=3)
 
-        save_btn = Button(master, text='Save', command=self.saveCanvas)
+        save_btn = Button(master, text="Save", command=self.saveCanvas)
         save_btn.grid(column=1, row=2)
 
         self.turtle = Tortue()
         self.turtle.image(self.canvasTurtle)
 
-        self.queue = queue
-        self.endApplication = endApplication
-
     def saveCanvas(self):
         self.turtle.canvas.delete(self.turtle.dessinImage)
-        self.canvasTurtle.postscript(file="image.eps", colormode='color')
+        self.canvasTurtle.postscript(file="image.eps", colormode="color")
 
-        #EpsImagePlugin.gs_windows_binary = r'C:\Program Files\gs\gs10.01.1\bin\gswin64c'
+        # EpsImagePlugin.gs_windows_binary = r'C:\Program Files\gs\gs10.01.1\bin\gswin64c'
 
-        #epsimage = Image.open('image.eps')
+        # epsimage = Image.open('image.eps')
         # epsimage.show()
-        #epsimage.save('image.png', quality=99)
+        # epsimage.save('image.png', quality=99)
 
-        self.turtle.dessinImage = self.turtle.canvas.create_image(self.turtle.coord[0], self.turtle.coord[1],
-                                                                  anchor=NW, image=self.turtle.imageFin)
+        self.turtle.dessinImage = self.turtle.canvas.create_image(
+            self.turtle.coord[0],
+            self.turtle.coord[1],
+            anchor=NW,
+            image=self.turtle.imageFin,
+        )
 
     def processIncoming(self, msg):
-        print("Graphical VIsualizer : " + msg)
         x = msg.split()
 
+        self.turtle.running = True
+
         if x[0] == "AVANCE":
-            self.turtle.avancer(0, int(x[1]),
-                                [(self.turtle.coord[0] + (self.turtle.coord[0] + 30)) / 2,
-                                 (self.turtle.coord[1] + (self.turtle.coord[1] + 30)) / 2])
+            self.turtle.avancer(
+                0,
+                int(x[1]),
+                [
+                    (self.turtle.coord[0] + (self.turtle.coord[0] + 30)) / 2,
+                    (self.turtle.coord[1] + (self.turtle.coord[1] + 30)) / 2,
+                ],
+            )
         elif x[0] == "RECULE":
-            self.turtle.reculer(0, int(x[1]),
-                                [(self.turtle.coord[0] + (self.turtle.coord[0] + 30)) / 2,
-                                 (self.turtle.coord[1] + (self.turtle.coord[1] + 30)) / 2])
+            self.turtle.reculer(
+                0,
+                int(x[1]),
+                [
+                    (self.turtle.coord[0] + (self.turtle.coord[0] + 30)) / 2,
+                    (self.turtle.coord[1] + (self.turtle.coord[1] + 30)) / 2,
+                ],
+            )
         elif x[0] == "TOURNEGAUCHE":
             self.turtle.tourner(-int(x[1]))
         elif x[0] == "TOURNEDROITE":
@@ -72,6 +80,8 @@ class GraphicalVisualizer:
             self.turtle.changerCouleur([int(x[1]), int(x[2]), int(x[3])])
         elif x[0] == "FCAP":
             self.turtle.fixerCap(int(x[1]))
-        elif x[0] == "QUITTER":
-            self.endApplication()
-            self.ivybus.stop()
+
+        self.turtle.running = False
+
+    def stop(self):
+        pass
