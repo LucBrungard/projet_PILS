a5e0f42d0e938bd1cacb2abed74d6ec0f21e35b0
LucBrungard <luc.brungard@laposte.net>
2023-04-27 17:36:41 +0200

    feat: :sparkles: add cli to main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from tkinter import *
+
 # from commands import *
 from editor.editor import *
 from graphicalVisualizer.graphicalVisualizer import *
 from textualVisualizer.textualVisualizer import *
 from queue import *
+from cli.cli import *
 
 
 class Main:
@@ -19,16 +21,46 @@ class Main:
         self.queue = Queue()
 
         self.ivybus = MyAgent("main")
+        self.registerBindings()
 
         self.createEditor()
         self.createGV()
         self.createTV()
-
-        self.registerBindings()
+        self.cli = CLI()
+        self.cli.start()
 
         self.periodicCall()
 
         mainloop()
+
+    def registerBindings(self):
+        self.ivybus.bind_msg(
+            lambda agent, n: self.queue.put("AVANCE " + n), "AVANCE (.*)"
+        )
+        self.ivybus.bind_msg(
+            lambda agent, n: self.queue.put("RECULE " + n), "RECULE (.*)"
+        )
+        self.ivybus.bind_msg(
+            lambda agent, n: self.queue.put("TOURNEDROITE " + n), "TOURNEDROITE (.*)"
+        )
+        self.ivybus.bind_msg(
+            lambda agent, n: self.queue.put("TOURNEGAUCHE " + n), "TOURNEGAUCHE (.*)"
+        )
+        self.ivybus.bind_msg(lambda agent: self.queue.put("LEVECRAYON"), "LEVECRAYON")
+        self.ivybus.bind_msg(
+            lambda agent: self.queue.put("BAISSECRAYON"), "BAISSECRAYON"
+        )
+        self.ivybus.bind_msg(lambda agent: self.queue.put("RESTAURE"), "RESTAURE")
+        self.ivybus.bind_msg(lambda agent: self.queue.put("NETTOIE"), "NETTOIE")
+        self.ivybus.bind_msg(lambda agent: self.queue.put("ORIGINE"), "ORIGINE")
+        self.ivybus.bind_msg(
+            lambda agent, x, y: self.queue.put(f"FPOS {x} {y}"), "FPOS (.*) (.*)"
+        )
+        self.ivybus.bind_msg(
+            lambda agent, r, g, b: self.queue.put(f"FCC {r} {g} {b}"),
+            "FCC (.*) (.*) (.*)",
+        )
+        self.ivybus.bind_msg(lambda agent, n: self.queue.put("FCAP " + n), "FCAP (.*)")
 
     def periodicCall(self):
         """
@@ -36,18 +68,24 @@ class Main:
         """
         if self.queue.qsize():
             try:
-                if not self.graphicalVisualizer.turtle.running:
-                    msg = self.queue.get(0)
+                if self.graphicalVisualizer != None:
+                    if not self.graphicalVisualizer.turtle.running:
+                        msg = self.queue.get(0)
 
-                    if (self.textualVisualizer != None):
-                        self.textualVisualizer.processIncoming(msg)
-                    if (self.graphicalVisualizer != None):
+                        if self.textualVisualizer != None:
+                            self.textualVisualizer.processIncoming(msg)
                         self.graphicalVisualizer.processIncoming(msg)
+                else:
+                    if self.textualVisualizer != None:
+                        msg = self.queue.get(0)
+                        self.textualVisualizer.processIncoming(msg)
             except Empty:
                 pass
 
         if not self.running:
             import sys
+
+            self.master.destroy()
             sys.exit(1)
         self.after = self.master.after(100, self.periodicCall)
 
@@ -59,7 +97,10 @@ class Main:
             self.master,
             text="Visualiseur Graphique",
             background="green",
-            command=lambda: self.createItem("graphicalVisualizer", "btnGV", GraphicalVisualizer))
+            command=lambda: self.createItem(
+                "graphicalVisualizer", "btnGV", GraphicalVisualizer
+            ),
+        )
         self.btnGV.grid(row=0, column=1)
 
         self.btnGV.invoke()
@@ -73,7 +114,10 @@ class Main:
             self.master,
             text="Visualiseur textuel",
             background="green",
-            command=lambda: self.createItem("textualVisualizer", "btnTV", TextualVisualizer))
+            command=lambda: self.createItem(
+                "textualVisualizer", "btnTV", TextualVisualizer
+            ),
+        )
         self.btnTV.grid(row=0, column=2)
 
         self.btnTV.invoke()
@@ -87,19 +131,28 @@ class Main:
             self.master,
             text="Editeur",
             background="green",
-            command=lambda: self.createItem("editor", "btnEditor", Editor))
+            command=lambda: self.createItem("editor", "btnEditor", Editor),
+        )
         self.btnEditor.grid(row=0, column=0)
 
         self.btnEditor.invoke()
 
     def closeWindow(self, btn: Button, window: Toplevel, itemName):
+        item = getattr(self, itemName)
+        item.stop()
+        item = None
+
         btn.configure(bg="red")
-        if (itemName == "editor"):
-            self.editor.ivybus.stop()
-        setattr(self, itemName, None)
+        setattr(self, itemName, item)
+
         window.destroy()
 
-    def createItem(self, itemName: str, btnName: str, constructor: Editor or GraphicalVisualizer or TextualVisualizer):
+    def createItem(
+        self,
+        itemName: str,
+        btnName: str,
+        constructor: Editor or GraphicalVisualizer or TextualVisualizer,
+    ):
         """Créer une fenêtre si elle n'existe pas encore
 
         Args:
@@ -112,50 +165,32 @@ class Main:
 
         if item == None:
             window = Toplevel(self.master)
-            window.protocol("WM_DELETE_WINDOW",
-                            lambda btn=btn, window=window: self.closeWindow(btn, window, itemName))
+            window.protocol(
+                "WM_DELETE_WINDOW",
+                lambda btn=btn, window=window: self.closeWindow(btn, window, itemName),
+            )
 
-            if (constructor == GraphicalVisualizer or constructor == TextualVisualizer):
-                item = constructor(
-                    window, self.queue, self.ivybus, self.endApplication)
-            else:
-                item = constructor(window)
+            item = constructor(window)
             setattr(self, itemName, item)
-            btn.configure(background='green')
+
+            btn.configure(background="green")
 
     def endApplication(self):
         self.ivybus.stop()
-        if (self.editor != None):
-            self.editor.ivybus.stop()
-        self.running = 0
 
-    def registerBindings(self):
-        self.ivybus.bind_msg(
-            lambda agent, n: self.queue.put("AVANCE " + n), "AVANCE (.*)")
-        self.ivybus.bind_msg(
-            lambda agent, n: self.queue.put("RECULE " + n), "RECULE (.*)")
-        self.ivybus.bind_msg(
-            lambda agent, n: self.queue.put("TOURNEDROITE " + n), "TOURNEDROITE (.*)")
-        self.ivybus.bind_msg(
-            lambda agent, n: self.queue.put("TOURNEGAUCHE " + n), "TOURNEGAUCHE (.*)")
-        self.ivybus.bind_msg(
-            lambda agent: self.queue.put("LEVECRAYON"), "LEVECRAYON")
-        self.ivybus.bind_msg(
-            lambda agent: self.queue.put("BAISSECRAYON"), "BAISSECRAYON")
-        self.ivybus.bind_msg(
-            lambda agent: self.queue.put("RESTAURE"), "RESTAURE")
-        self.ivybus.bind_msg(
-            lambda agent: self.queue.put("NETTOIE"), "NETTOIE")
-        self.ivybus.bind_msg(
-            lambda agent: self.queue.put("ORIGINE"), "ORIGINE")
-        self.ivybus.bind_msg(
-            lambda agent, x, y: self.queue.put(f"FPOS {x} {y}"), "FPOS (.*) (.*)")
-        self.ivybus.bind_msg(
-            lambda agent, r, g, b: self.queue.put(f"FCC {r} {g} {b}"), "FCC (.*) (.*) (.*)")
-        self.ivybus.bind_msg(
-            lambda agent, n: self.queue.put("FCAP " + n), "FCAP (.*)")
-        self.ivybus.bind_msg(
-            lambda agent: self.queue.put("QUITTER"), "QUITTER")
+        if self.editor != None:
+            self.editor.stop()
+
+        if self.textualVisualizer != None:
+            self.textualVisualizer.stop()
+
+        if self.graphicalVisualizer != None:
+            self.graphicalVisualizer.stop()
+
+        if self.cli != None:
+            self.cli.stop()
+
+        self.running = 0
 
 
 Main()
